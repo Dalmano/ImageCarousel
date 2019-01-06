3fca630e78b57ace7a4037cfcb1b91e8e19beb93
Dylan A <30301023+Dalmano@users.noreply.github.com>
2019-01-06 11:42:57 +0000

    Update untitled.js

--- untitled.js
+++ untitled.js
@@ -114,35 +114,6 @@ pauseButton.onclick = function() {
 showSlides();
 
 
-(function(){
- var slide = 0,  // Current slide 
-     max   = 4, // Maximum Number of slides
- ele = document.getElementById("slider");
-     document.addEventListener('keydown',function(e){
-          e.preventDefault();
-          e.stopPropagation(); // To stop more event stuff and default behaviour
-          key = e.keyCode; // To find out what key is this 
-          if( key === 39 ){
-              // Right arrow 
-              // Aha we incremented the value!
-              slide++;
-              slide %= (max+1);
-              // Increase the value of slide by 1 and keep em in limits
-          }else if( key === 37){
-              slide = (--slide >= 0)?slide: ( slide + max );
-              // Will decrement the slide value by 1 and if they are less then 0 then   will cycle it to the last slide 
-          }  
-
-          ele.className.replace(/slide[0-4]/gi,'slide'+slide);                 
-          // wasn't hard now was it ?     
-     });
- })();
-
-
-
-
-
-
 
 
 
